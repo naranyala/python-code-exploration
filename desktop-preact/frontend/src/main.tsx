631d824e6a12a0d7ac026dbed61745e77faa2742
naranyala <fudzer.mh@gmail.com>
2025-08-14 21:34:21 +0700

    update

--- desktop-preact/frontend/src/main.tsx
+++ desktop-preact/frontend/src/main.tsx
@@ -3,6 +3,12 @@ import { render } from 'preact'
 import './index.css'
 import './css-reset.css'
 
-import { App } from './app.tsx'
+// import { App } from './app.tsx'
+// render(<App />, document.getElementById('app')!)
 
-render(<App />, document.getElementById('app')!)
+// import { Sample } from './apps/Sample.jsx'
+// render(<Sample />, document.getElementById('app')!)
+
+
+import { Sample2 } from './apps/Sample2.jsx'
+render(<Sample2 />, document.getElementById('app')!)
--- desktop-vuejs/another_v2.py
+++ desktop-vuejs/another_v2.py
@@ -43,7 +43,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # html_source = "./templates/io-jobportal2/index.html"
 # html_source = "./templates/docs-python/index.html"
 # html_source = "./templates/todo-app/index.html"
-html_source = "./templates/studio/index.html"
+# html_source = "./templates/studio/index.html"
+# html_source = "./templates/endless-grid/index.html"
+html_source = "./templates/star-wars-opening/index.html"
 
 html_file_path = os.path.join(current_dir, html_source)
 
